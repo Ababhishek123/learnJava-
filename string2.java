8cc3b54e58bfcc4a6fa03939435c3ae9a128f1b1
Abhishek Bharti <66540907+Ababhishek123@users.noreply.github.com>
2021-09-24 13:54:28 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,17 @@
+package com.meetTrivia;
+
+public class string2 { 
+    
+    public static void main(String[] arg){
+        System.out.println("hero");        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
